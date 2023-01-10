3527cd8800020f16ca1037b2971bffe186ce3dcc
Adam Jensen <adam@acj.sh>
2023-01-10 07:31:33 -0500

    Upgrade clap dependency to 4.x

--- src/bin.rs
+++ src/bin.rs
@@ -10,7 +10,7 @@ const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 fn main() -> Result<()> {
     let num_cores = num_cpus::get_physical();
-    let num_cores_for_display = format!("{}", num_cores);
+    let num_cores_for_display: &'static str = Box::leak(format!("{}", num_cores).into_boxed_str());
     let cmd = Command::new("krapslog")
         .version(VERSION)
         .about("Visualize log files using sparklines")
@@ -25,7 +25,6 @@ fn main() -> Result<()> {
                 .short('F')
                 .long("format")
                 .help("Timestamp format to match")
-                .takes_value(true)
                 .required(false)
                 .default_value("%d/%b/%Y:%H:%M:%S%.f"),
         )
@@ -34,8 +33,8 @@ fn main() -> Result<()> {
                 .short('m')
                 .long("markers")
                 .help("Number of time markers to display")
-                .takes_value(true)
                 .required(false)
+                .value_parser(clap::value_parser!(usize))
                 .default_value("0"),
         )
         .arg(
@@ -43,25 +42,25 @@ fn main() -> Result<()> {
                 .short('c')
                 .long("concurrency")
                 .help("Number of threads to use when processing large files (defaults to number of CPU cores)")
-                .takes_value(true)
                 .required(false)
-                .default_value(&num_cores_for_display),
+                .value_parser(clap::value_parser!(usize))
+                .default_value(num_cores_for_display),
         );
     let arg_matches = cmd.get_matches();
 
-    let timestamp_format = arg_matches.value_of("FORMAT").unwrap();
-    let timestamps = match arg_matches.value_of("FILE") {
+    let timestamp_format: &String = arg_matches.get_one::<String>("FORMAT").unwrap();
+    let timestamps = match arg_matches.get_one::<String>("FILE") {
         None => {
             if atty::is(atty::Stream::Stdin) {
                 eprintln!("Reading from standard input. Paste your log and then send EOF (e.g. by pressing ctrl-D).");
             }
 
-            krapslog::scan_for_timestamps(std::io::stdin(), timestamp_format)
+            krapslog::scan_for_timestamps(std::io::stdin(), &timestamp_format)
         }
         Some(filename) => {
             let file = fs::File::open(filename)?;
             let chunker = FileChunker::new(&file)?;
-            let mut count = arg_matches.value_of_t("CONCURRENCY").unwrap();
+            let mut count = *arg_matches.get_one("CONCURRENCY").unwrap();
             if file.metadata()?.len() < 10 * 1024 * 1024 {
                 count = 1;
             }
@@ -69,7 +68,7 @@ fn main() -> Result<()> {
             Ok(chunker
                 .chunks(count, Some('\n'))?
                 .into_par_iter()
-                .map(|chunk| krapslog::scan_for_timestamps(chunk, timestamp_format))
+                .map(|chunk| krapslog::scan_for_timestamps(chunk, &timestamp_format))
                 .filter_map(Result::ok)
                 .collect::<Vec<Vec<i64>>>()
                 .concat())
@@ -85,7 +84,7 @@ fn main() -> Result<()> {
         _ => 80,
     };
 
-    let num_markers = arg_matches.value_of_t("MARKERS")?;
+    let num_markers: usize = *arg_matches.get_one("MARKERS").unwrap();
     let (header, footer) = krapslog::build_time_markers(&timestamps, num_markers, terminal_width);
     let sparkline = krapslog::build_sparkline(&timestamps, terminal_width)?;
     print!("{}", header);
